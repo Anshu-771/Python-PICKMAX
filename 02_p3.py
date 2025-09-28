371f576575422f098bc12eb20a4ba606f6d194b5
Anshu Kumar <anshubhardwaj919@gmail.com>
2025-09-28 14:09:50 +0530

    Chaper two Completed Successfully : MOD 1

--- 02_p3.py
+++ 02_p3.py
@@ -11,5 +11,5 @@ print("Average of given Numbers are : ",c)
 # Q6
 # Write a programme to calculate the square of a number entered by the user
 d = int(input("Enter the Number : "))
-e = d*d
+e = d*d          # for like you want to check a^10 we prefer this a**10 again this a*a*a*a*a*a*a*a*a*a
 print("Square of Number you enter is : ",e)
