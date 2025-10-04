6692a6d4a17586d395f2f4d2bd83c2c586eb39a2
Anshu Kumar <anshubhardwaj919@gmail.com>
2025-10-04 16:38:56 +0530

    learn 08 and practice set

---
+++ 08_p3.py
@@ -0,0 +1,29 @@
+# Function and Recursions
+
+# A function is a group of statements perform a specific task.
+# When a program gets bigger in a size and it is complex grows it gets difficulty for a program to keep track on which piece of code is doing what!
+# A function can be reused by a programmer in a given program any number of times.
+
+# syntax of function defination # The part containing the exact set of instructions which are executed during the function call.
+# def function_name(arguments):
+#   Statement for execution.
+
+# syntax of function call  # Whenever we want to call a function we put the name of the function followed by a parenthesis.
+# function_name(arguments)
+
+# There are two types of function (build-in function /user-defined function)
+# Build-in function are already present in the python  ex: len(),range() etc.
+# User defined function defined by the user ex: func1().
+
+# A function can accept some value it can work with, we can put this value in the parenthesis it is known as argument 
+# Function can also return the value.
+
+# Default argument
+# We can have a value default as default argument in a function.
+# If we specify name = "stranger" in the line containing def these value is used when no argument is passed.
+
+# Recursion
+# Recursion is a function which call itself , it is used to directly use a mathematical formula as a function.
+# ex : factorial(n) = n * factorial(n-1)
+# The programmer needs to be extremely careful while working with recursion to ensure that the function does not Infinite keep calling itself.
+# Recursion is Sometimes the most direct way to code an algorithm.
