cc18abff51715fce0d0003fa80d2d6c3ef228547
Anshu Kumar <anshubhardwaj919@gmail.com>
2025-10-04 13:07:40 +0530

    learn 07 and practice set

---
+++ 07_p3.py
@@ -0,0 +1,20 @@
+#loops in python
+
+# for loop
+# for loop is used to iterate over a sequence (like a list, tuple, dictionary, set, or string)
+# for loop is used when the number of iterations is known
+# for loop itrates by using a variable that takes the value of the item inside the sequence on each iteration
+# for loop uses the range() function to generate a sequence of numbers without this function we can not use for loop to iterate over a sequence of numbers
+# syntax of for loop
+# for variable in sequence:
+#     #code to be executed
+#     print(variable)
+
+# While loop
+# while loop is used to iterate over a block of code as long as the condition is true
+# while loop is used when the number of iterations is not known
+# While loop uses a condition to check whether to continue the loop or not
+# syntax of while loop
+# while condition:
+#     #code to be executed
+#     print(variable)
